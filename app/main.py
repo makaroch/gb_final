0668701c9b5552f266a9e401a152618f8c67323d
Alex <da.ek.ned@yandex.ru>
2024-01-17 05:49:58 +0300

    add ozon handles

--- app/main.py
+++ app/main.py
@@ -33,6 +33,20 @@ async def get_wb_products(request: RequestParsMany) -> list[list[ProductCard]]:
     return await WildberriesParser(request).get_many_product()
 
 
+@app.get("/ozon_product")
+async def get_ozon_product(search: str, city: str = "Москва",
+                           sorting: Sorts = Sorts.popular,
+                           delivery_time: DeliveryTime = DeliveryTime.five_days,
+                           min_price: int = 100,
+                           max_prise: int = 1000000,
+                           quantity: int = 10) -> list[ProductCard] | list:
+    return [ProductCard()]
+
+
+@app.post("/ozon_products")
+async def get_wb_products(request: RequestParsMany) -> list[list[ProductCard]]:
+    return [[ProductCard()], [ProductCard()]]
+
 
 if __name__ == "__main__":
     uvicorn.run(app=app,
